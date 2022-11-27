a07ea9f2897767ec28e4c6d68277c042f32e9e05
shiva748 <shivagautam2002@gmail.com>
2022-11-27 16:27:51 +0530

    update

--- client/src/Components/Pages/Cbooking.js
+++ client/src/Components/Pages/Cbooking.js
@@ -583,6 +583,7 @@ const Cbooking = () => {
           }
         />
         <link rel="canonical" href="http://revacabs.com/" />
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://checkout.razorpay.com/v1/checkout.js"></meta>
         <meta name="robots" content="noindex,nofollow" />
       </Helmet>
       <div className="page-head">
