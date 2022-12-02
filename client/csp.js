cfb07af1f2af065671459f17b233d0d974313d00
shiva748 <shivagautam2002@gmail.com>
2022-12-03 01:02:05 +0530

    update

--- client/csp.js
+++ client/csp.js
@@ -8,28 +8,17 @@ module.exports = {
     ]
     },
     prod: {
-    "default-src": "'self'",  // can be either a string or an array.
+    "default-src": [
+        "'self'",
+        "https://*.facebook.com",
+        "https://*.razorpay.com",
+        "unsafe-inline"
+      ],  // can be either a string or an array.
     "style-src": [
       "'self'",
       "https://*.facebook.com",
-      "'unsafe-inline'"
-    ],
-    "script-src":[
-        "'self'",
-      "https://*.google.com",
       "https://*.razorpay.com",
-      "'unsafe-inline'"
-    ],
-    "connect-src": [
-      "'self'",
-      "https://*.razorpay.com",
-      "'unsafe-inline'"
-    ],
-    "img-src":[
-        "'self'",
-        "https://*.google.com",
-        "https://*.razorpay.com",
-        "'unsafe-inline'"
+      "unsafe-inline"
     ]
     }
   }
