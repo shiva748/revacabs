d9f6e1a26e46652216b71b821b7985faa35f70ef
shiva748 <shivagautam2002@gmail.com>
2022-12-10 15:00:43 +0530

    update

--- controller/clientcontroller.js
+++ controller/clientcontroller.js
@@ -1146,13 +1146,9 @@ exports.crt_booking = async (req, res) => {
   } else if (!["Personal", "Business"].some((itm) => itm === trprsn)) {
     return res.status(422).json("Invalid trip reason selected");
   }
-  console.log(new Date(pickupat+ 19800000).toLocaleDateString('en-IN') !==
-  new Date(pickupdate).toLocaleDateString('en-IN'))
-  console.log(new Date(pickupat+19800000).toLocaleDateString('en-IN'),
-  new Date(pickupdate).toLocaleDateString('en-IN'))
   if (
-    new Date(pickupat+19800000).toLocaleDateString('en-IN') !==
-    new Date(pickupdate).toLocaleDateString('en-IN')
+    new Date(pickupat).toLocaleDateString({timeZone: 'Asia/Kolkata'})!==
+    new Date(pickupdate).toLocaleDateString({timeZone: 'Asia/Kolkata'})
   ) {
     return res.status(422).json("Invalid Pickup Date & Time");
   }
